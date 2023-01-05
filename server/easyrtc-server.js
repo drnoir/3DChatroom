52b4a66e7899e8a83678b9e155eeabc9c5f8ff18
drnoir <chrispgodber@gmail.com>
2023-01-05 20:44:41 +0000

    updated server serve path

--- server/easyrtc-server.js
+++ server/easyrtc-server.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === "development") {
 }
 
 // Serve the files from the examples folder
-app.use(express.static(path.resolve(__dirname, "..", "examples")));
+app.use(express.static(path.resolve(__dirname, "..", "chat")));
 
 // Start Express http server
 const webServer = http.createServer(app);
